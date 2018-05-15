4ebee9d5561f4612752d585270dba79536368c23
egora34 <39244160+egora34@users.noreply.github.com>
2018-05-15 18:38:44 +0300

    Update conftest.py

--- auto_tests/conftest.py
+++ auto_tests/conftest.py
@@ -1,16 +1,24 @@
 import pytest
 import time
 from selenium import webdriver
+import csv
 
 driver = webdriver.Chrome()
 
+# def pytest_addoption(parser):
+# parser.addoption("--file", action="append", default=None)
+
+# def pytest_generate_tests(metafunc):
+# 
+
 @pytest.fixture(scope="class", autouse=True)
 def resource_a_setup(request):
+    print('8622634' * 12)
     driver.maximize_window()
     request.cls.driver = driver
-
+    
     def resource_a_teardown():
         time.sleep(1)
         driver.quit()
-
+        
     request.addfinalizer(resource_a_teardown)
